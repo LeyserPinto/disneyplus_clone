f1b59f75533fc7de623d8c142604da4e86c58e8f
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2023-04-21 00:04:58 -0300

    M21: uglify config

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp      = require('gulp');
 const sass      = require('gulp-sass')(require('sass'));
 const imagemin  = require('gulp-imagemin');
-
+const uglify    = require('gulp-uglify');
 /**
  * Build estilos 
  * @returns gulp_function
@@ -10,6 +10,12 @@ function baked_styles () {
     return gulp.src('./src/styles/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
+}
+
+function baked_scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
 }
 /**
  * Comprime as Imagens
@@ -21,7 +27,8 @@ function image_min () {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(baked_styles, image_min);
+exports.default = gulp.parallel(baked_styles, image_min, baked_scripts);
 exports.watch   = function () {
     gulp.watch("./src/styles/*.scss", gulp.parallel(baked_styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(baked_scripts));
 }
